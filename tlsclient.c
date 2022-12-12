cd02677a8eda3ba6af619581e43e3df3e070fe2c
Abhishek Choubey <mail.akchoubey@gmail.com>
2022-12-13 01:25:00 +0530

    Establish a TCP connection with the server

--- tlsclient.c
+++ tlsclient.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -17,13 +18,8 @@ typedef struct ip4sock_s
 } ip4sock_t;
 
 /* Open a TCP socket */
-static int tcp_sock_init(ip4sock_t *s)
+static int tcp_init(ip4sock_t *s)
 {
-	if (!strlen(s->ip) || !s->port) {
-		printf("Invalid IP/Port.\n");
-		return 1;
-	}
-
 	int type = -1;
 	type = ((s->type == AF_INET) || (s->type == AF_INET6));
 	if (!type) {
@@ -34,7 +30,7 @@ static int tcp_sock_init(ip4sock_t *s)
 	int fd = -1;
 	fd = socket(s->type, SOCK_STREAM, 0);
 	if (fd < 0) {
-		perror("tcp_sock_init_socket");
+		perror("tcp_init_socket");
 		return 1;
 	}
 
@@ -42,10 +38,31 @@ static int tcp_sock_init(ip4sock_t *s)
 	int optval = 1;
 	socklen_t slen = sizeof(struct sockaddr_in);
 	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optval, slen) < 0) {
-		perror("tcp_sock_init_setsockopt");
+		perror("tcp_init_setsockopt");
 		return 1;
 	}
 	s->fd = fd;
+	return 0;
+}
+
+static int tcp_connect(ip4sock_t *s)
+{
+	if (!s || (s->fd <= 0))
+	{
+		printf("Invalid socket\n");
+		return 1;
+	}
+	struct sockaddr_in server = {0};
+	socklen_t slen = sizeof(server);
+	server.sin_family = s->type;
+	server.sin_port = htons(s->port);
+	server.sin_addr.s_addr = inet_addr(s->ip);
+	if (connect(s->fd, (struct sockaddr *)&server, slen) < 0)
+	{
+		perror("tcp_connect");
+		return 1;
+	}
+	close(s->fd);
 	return 0;
 }
 
@@ -55,10 +72,15 @@ int main(void)
 	snprintf(tcpsock.ip, sizeof(tcpsock.ip), "127.0.0.1");	
 	tcpsock.port = 2083;
 	tcpsock.type = AF_INET;
-	if (tcp_sock_init(&tcpsock)) {
+	if (tcp_init(&tcpsock)) {
 		printf("Failed to initialize tcp socket for ip:port - %s:%d\n", tcpsock.ip, tcpsock.port);
 		return 1;
 	}
-	printf("TCP socket initialized for ip:port - %s:%d\n", tcpsock.ip, tcpsock.port);
+	printf("Processing TCP socket for ip:port - %s:%d\n", tcpsock.ip, tcpsock.port);
+	printf("Socket initialized.\n");
+	if (tcp_connect(&tcpsock)) {
+		printf("Failed to connect");
+		return 1;
+	}
 	return 0;
 }
